3ecc3d1ff7c09c1a826a7c7da1fd55a08051886a
ZainUrRehmanKhan <zainkhan.official10@gmail.com>
2023-10-13 18:45:00 +0500

    :hammer: port changed

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  server: {
+    port: 3333, // Change this to your desired port
+  },
 }
 module.exports = nextConfig
